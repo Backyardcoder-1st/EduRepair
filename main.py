86171de0c684c177387cd5210f43c36bfba81d82
Backyardcoder-1st <tienkyler@gmail.com>
2026-07-14 22:49:08 +0700

    sua

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
 import flet as ft
 from logic import AppController
 import os
+import mimetypes
 
+mimetypes.add_type("application/javascript", ".js")
+mimetypes.add_type("application/javascript", ".mjs")
 
 def main(page: ft.Page):
-
     page.title = "Student Management System"
-
     page.window_width = 900
     page.window_height = 650
 
-
+    # Cấu hình trang theo style Light-Theme của bạn
     page.theme_mode = ft.ThemeMode.LIGHT
-
     page.bgcolor = ft.Colors.WHITE
-
-
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-
+    # Khởi tạo lớp điều khiển ứng dụng
     app = AppController(page)
 
-
+    # Thêm thành phần đồ họa gốc vào giao diện trước
     page.add(app.root)
 
-
+    # Kích hoạt tải dữ liệu nền
     app.start()
 
 
-
-# Render will provide an environment variable called 'PORT'.
-# If it's not found (like when you run it locally), it defaults to 8080.
-port = int(os.getenv("PORT", 8080))
-
-ft.app(
-    target=main,
-    host="0.0.0.0",  # Tells Flet to accept connections from the public web
-    port=port
-)
+if __name__ == "__main__":
+    ft.app(
+        target=main,
+        view=ft.AppView.WEB_BROWSER
+    )
